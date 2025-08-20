526209a07136c6d7e5bc2cf93b5b4d6171de8189
halityrd <halityrd@hotmail.com>
2025-08-20 22:48:57 +0300

    JSX - CSS

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -28,7 +28,19 @@ const Image = () => (
 );
 const Title = () => <h2>Interesting Facts For Curious Minds</h2>;
 const Author = () => {
-  return <h4>Jordan Moore</h4>;
+  const inlineHeadingStyles = {
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '0.5rem',
+  };
+
+  return <h4 style={inlineHeadingStyles}>Jordan Moore</h4>;
+
+  // return (
+  //   <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>
+  //     Jordan Moore
+  //   </h4>
+  // );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
