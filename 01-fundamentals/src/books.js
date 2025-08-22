f7d81687950184a0b09e413b3268b129e82399e6
halityrd <halityrd@hotmail.com>
2025-08-22 03:52:09 +0300

    ES6 Modules

---
+++ 01-fundamentals/src/books.js
@@ -0,0 +1,16 @@
+export const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interest Facts For Curios Minds',
+    img: './images/book-1.jpg',
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: './images/book-2.jpg',
+    id: 2,
+  },
+];
+
+// export default books;
--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -2,54 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-
-const books = [
-  {
-    author: 'Jordan Moore',
-    title: 'Interest Facts For Curios Minds',
-    img: './images/book-1.jpg',
-    id: 1,
-  },
-  {
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: './images/book-2.jpg',
-    id: 2,
-  },
-];
+import { books } from './books';
+import Book from './Book';
 
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id);
-    console.log(book);
-  };
-  // getBook(2);
-
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
-  );
-};
-
-const Book = (props) => {
-  const { img, title, author, getBook, id } = props;
-  // console.log(props);
-
-  const getSingleBook = () => {
-    getBook(id);
-  };
-
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      {/* <button onClick={getSingleBook}>click me</button> */}
-      <button onClick={() => getBook(id)}>display title</button>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
