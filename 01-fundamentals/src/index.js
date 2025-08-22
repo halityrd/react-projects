ca3622216feb963017782f7d953cb1d40450fce2
halityrd <halityrd@hotmail.com>
2025-08-22 03:23:51 +0300

    Components Feature

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -28,34 +28,19 @@ const BookList = () => {
   );
 };
 
-const EventExamples = () => {
-  return (
-    <section>
-      <form>
-        <h2>Typical Form</h2>
-        <input
-          type="text"
-          name="example"
-          style={{ margin: '1rem 0' }}
-          onChange={(e) => console.log(e.target.value)}
-        />
-        <button type="submit">click me</button>
-      </form>
-      <button onClick={() => console.log('click me')} type="button">
-        click me
-      </button>
-    </section>
-  );
-};
-
 const Book = (props) => {
-  console.log(props);
   const { img, title, author } = props;
+  // console.log(props);
+
+  const displayTitle = () => {
+    console.log(title);
+  };
 
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
+      <button onClick={displayTitle}>display title</button>
       <h4>{author}</h4>
     </article>
   );
