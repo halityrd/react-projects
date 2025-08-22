a97837f75255bbaafff3f2a269468e5deacd9b26
halityrd <halityrd@hotmail.com>
2025-08-22 04:01:45 +0300

    Numbers Challenge

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
+// import books from './books';
 import { books } from './books';
 import Book from './Book';
 
 const BookList = () => {
   return (
     <section className="booklist">
-      {books.map((book) => {
-        return <Book {...book} key={book.id} />;
+      {books.map((book, index) => {
+        return <Book {...book} key={book.id} number={index} />;
       })}
     </section>
   );
